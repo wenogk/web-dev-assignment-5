042d5e99db96599cf3407b8ad9dfd1869c673150
Romeno Wenogk Fernando <wenogk@nyu.edu>
2021-11-12 22:43:16 -0500

    added global constants that are valid for all views such as sidebar info like phone, address, name etc.

---
+++ views/constants/global.constants.js
@@ -0,0 +1,7 @@
+module.exports = {  
+    name: 'Dunumalage Romeno Wenogk Fernando',
+    birthday : "May 7th 1999",
+    phone : "+1 123 456 7890",
+    email : "wenogk@gmail.com",
+    location : "Waterloo, ON, Canada"
+};
