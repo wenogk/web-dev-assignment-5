5b68a8548557becaf5488f1d983ec09b53643a57
Romeno Wenogk Fernando <wenogk@nyu.edu>
2021-11-14 12:45:15 -0500

    huge restructuring to have partial views for header, footer and sidebar components to be available separately

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,23 @@ var express = require('express');
 var router = express.Router();
 var indexConstants = require('../views/constants/index.constants');
 var portfolioConstants = require('../views/constants/portfolio.constants');
+var resumeConstants = require('../views/constants/resume.constants');
+var contactConstants = require('../views/constants/contact.constants');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', indexConstants);
 });
 
+router.get('/resume', function(req, res, next) {
+  res.render('resume', resumeConstants);
+});
+
 router.get('/portfolio', function(req, res, next) {
   res.render('portfolio', portfolioConstants);
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', contactConstants);
 });
 
 module.exports = router;
