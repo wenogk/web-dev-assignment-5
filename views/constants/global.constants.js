a09e424837bfecb168c395e44fb839db315f8575
Romeno Wenogk Fernando <wenogk@nyu.edu>
2021-11-14 23:39:42 -0500

    moved aboutInfo to globalConstants

--- views/constants/global.constants.js
+++ views/constants/global.constants.js
@@ -8,5 +8,6 @@ module.exports = {
     githubLink: "https://github.com/wenogk",
     linkedInLink: "https://www.linkedin.com/in/wenogk/",
     facebookLink: "https://www.facebook.com/iwenogk",
-    twitterLink: "https://twitter.com/romeno_wenogk"
+    twitterLink: "https://twitter.com/romeno_wenogk",
+    aboutInfo: "Over 2 years of Backend and Fullstack software engineering experience with a B.S in Computer Science at New York University Abu Dhabi. I'm passionate about web technologies and have experience working with Node.js, ASP.NET Core, Flask, React, Typescript, MySQL, Postgres, MongoDB. Feel free to check out my Github to see my projects. I'm only one message away if you want to write me with any info, and job offers on the remote or in-person form."
 };
--- views/constants/portfolio.constants.js
+++ views/constants/portfolio.constants.js
@@ -3,6 +3,4 @@ var globalConstants = require('./global.constants')
 module.exports = { 
     ...globalConstants,
     activeNavigation : { portfolio : true},
-    title: 'Romeno\'s Portfolio',
-    aboutInfo: "Over 2 years of Backend and Fullstack software engineering experience with a B.S in Computer Science at New York University Abu Dhabi. I'm passionate about web technologies and have experience working with Node.js, ASP.NET Core, Flask, React, Typescript, MySQL, Postgres, MongoDB. Feel free to check out my Github to see my projects. I'm only one message away if you want to write me with any info, and job offers on the remote or in-person form."
-};
+    title: 'Romeno\'s Portfolio'};
